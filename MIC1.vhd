69c03510006648d10fb5adaeb732b38021c1c43a
jecrs687 <45917859+jecrs687@users.noreply.github.com>
2019-09-09 23:23:16 -0300

    Add files via upload

--- MIC1.vhd
+++ MIC1.vhd
@@ -4,7 +4,8 @@ use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
 entity MIC1 is
-	port (	clk:in std_logic;
+	port (	
+		clk:in std_logic;
 		MBR_reg : inout std_logic_vector(15 downto 0);
 		MAR_reg : out std_logic_vector(11 downto 0);
 		sigA : in std_logic_vector(3 downto 0);
@@ -25,7 +26,6 @@ entity MIC1 is
 		alu : in std_logic_vector(1 downto 0);
   		mbr_signal :in std_logic;
 		mar_signal :in std_logic
-		
 	);
 end MIC1;
 
@@ -68,6 +68,24 @@ begin
 			register_WR<=register_WR; 
 		end if;
 	end process;
+-----------------------------------------------------------------------------------------------------------------
+	
+with alu select
+ULAresult <=saidaAmux  + B when "00",
+saidaAmux and B when "01",
+saidaAmux when "10",
+not saidaAmux when others;
+-----------------------------------------------------------------------------------------------------------------
+with A0 select -- AMUX
+saidaAmux <= barA when '0',
+MBR_reg when '1',
+saidaAmux when others;  
+-----------------------------------------------------------------------------------------------------------------
+with SH select
+barC <= ULAresult when "00",
+ULAresult(14 downto 0)&'0' when "01", --desloca 1bit para a direita 
+'0' & ULAresult(15 downto 1) when "10", --desloca 1bit para a esquerda
+barC when others;
 -----------------------------------------------------------------------------------------------------------------	
         --case que controla a entrada do barramento A
 	with sigA select
@@ -106,18 +124,12 @@ begin
 		D when "1101" ,
 		E when "1110" ,
 		F when others;
------------------------------------------------------------------------------------------------------------------
-	with A0 select -- AMUX
-		saidaAmux <= barA when '0',
-		MBR_reg when '1',
-		saidaAmux when others;  
-	
-		
+
 	--PROCESS QUE CONTROLA O BARRAMENTO C PASSAR OU NÃO DADOS PARA OS REGISTRADORES
 -----------------------------------------------------------------------------------------------------------------	
 	process(clk)
 		begin
-		if(rising_edge(clk) AND ENC='1') then
+		if(clk'event and clk='1' AND ENC='1') then
 			case sigC is
 				when "0000" =>  PC   <= barC;
 				when "0001" =>  AC   <= barC;
@@ -140,34 +152,15 @@ begin
 		if (rising_edge(clk) and MAR_signal='1') then
 				MAR_reg <= barB(11 downto 0);
 		end if;
-	end process;
------------------------------------------------------------------------------------------------------------------
-	
-	with alu select
-		ULAresult <=saidaAmux  + B when "00",
-		saidaAmux and B when "01",
-		saidaAmux when "10",
-		not saidaAmux when others;
+	end process;  
 -----------------------------------------------------------------------------------------------------------------	 
-	process(clk)
-	begin
-	if(rising_edge(clk)) then
-	if (ULAresult = "0000000000000000") then
-	Z <= '1';
-	elsif (ULAresult(15) = '1') then
-	N <= '1';
-	else
-		N<='0';
-		Z<='0';
-	end if;
-	end if;
-	end process;
------------------------------------------------------------------------------------------------------------------
-	with SH select
-		barC <= ULAresult when "00",
-		ULAresult(14 downto 0)&'0' when "01", --desloca 1bit para a direita 
-		'0' & ULAresult(15 downto 1) when "10", --desloca 1bit para a esquerda
-		barC when others;  
+with ULAresult select
+	z<= '1' when "0000000000000000",
+	'0' when others;
+with ULAresult(15) select
+		N<= '1' when '1', 
+	'0' when others;
+
 -----------------------------------------------------------------------------------------------------------------
 	process(clk)
 		begin
